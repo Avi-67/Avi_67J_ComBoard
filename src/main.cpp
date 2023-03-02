3cb354a67d22da127ea15411ffd38ac35b46392d
watanabe1101 <wa.so1014@gmail.com>
2023-03-02 14:52:47 +0900

    a

--- src/main.cpp
+++ src/main.cpp
@@ -12,13 +12,14 @@ SoftwareSerial Serial5;
 SPICREATE::SPICreate SPIC;
 Flash flash1;
 
+class uart {
+  
+}
+
 void setup() {
-  Serial.begin(115200);
-  Serial.println("ESP launched");
+  Serial.begin(9600, SERIAL_8N1, RX3, TX3)
   Serial1.begin(115200, SERIAL_8N1, RX1, TX1); //twelite
   Serial2.begin(115200, SERIAL_8N1, RX2, TX2);//GPS RX:17 TX:16
-  Serial3.begin(9600, SWSERIAL_8N1, RX3, TX3, false, 256);//para
-  Serial3.enableIntTx(false);
   Serial4.begin(9600, SWSERIAL_8N1, RX4, TX4, false, 256);//log
   Serial4.enableIntTx(false);
   Serial5.begin(9600, SWSERIAL_8N1, RX5, TX5, false, 256);//LED
@@ -28,29 +29,32 @@ void setup() {
 void loop() {
   if(Serial1.available()){
     char cmd = Serial1.read();
-
-    Serial3.listen();
+    Serial1.print(cmd);
+    Serial1.println(" from twelite");
     Serial3.print(cmd);
-    while(Serial3.available()){
-      char cmd3 = Serial3.read();
-      Serial1.write(cmd3);
-      Serial1.println(" from para");
-    }
-
-    Serial4.listen();
     Serial4.print(cmd);
-    while(Serial4.available()){
-    char cmd4 = Serial4.read();
-    Serial1.write(cmd4);
-    Serial1.println(" from log");
-    }
-
-    Serial5.listen();
     Serial5.print(cmd);
-    while(Serial5.available()){
-      char cmd5 = Serial5.read();
-      Serial1.write(cmd5);
-      Serial1.println(" from led");
-    }
+  }
+
+  if(Serial.available()){
+    char cmd3 = Serial.read();
+    Serial.write(cmd3);
+    Serial.println(" from para")
+  }
+
+  Serial4.listen();
+  if(Serial4.available()){
+  char cmd4 = Serial4.read();
+  Serial4.print('j');
+  Serial1.write(cmd4);
+  Serial1.println(" from log");
+  }
+
+  Serial5.listen();
+  if(Serial5.available()){
+    Serial5.print('j');
+    char cmd5 = Serial5.read();
+    Serial1.write(cmd5);
+    Serial1.println(" from led");
   }
 }   
