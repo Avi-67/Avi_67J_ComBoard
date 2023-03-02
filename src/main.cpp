d34e549fbc2086b18d9c9a8b6aadeb5ead053c09
watanabe1101 <wa.so1014@gmail.com>
2023-03-02 15:01:09 +0900

    a

--- src/main.cpp
+++ src/main.cpp
@@ -12,12 +12,9 @@ SoftwareSerial Serial5;
 SPICREATE::SPICreate SPIC;
 Flash flash1;
 
-class uart {
-  
-}
 
 void setup() {
-  Serial.begin(9600, SERIAL_8N1, RX3, TX3)
+  Serial.begin(9600, SERIAL_8N1, RX3, TX3);
   Serial1.begin(115200, SERIAL_8N1, RX1, TX1); //twelite
   Serial2.begin(115200, SERIAL_8N1, RX2, TX2);//GPS RX:17 TX:16
   Serial4.begin(9600, SWSERIAL_8N1, RX4, TX4, false, 256);//log
@@ -39,7 +36,7 @@ void loop() {
   if(Serial.available()){
     char cmd3 = Serial.read();
     Serial.write(cmd3);
-    Serial.println(" from para")
+    Serial.println(" from para");
   }
 
   Serial4.listen();
